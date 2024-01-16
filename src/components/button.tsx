c58fed0c85cb4d3086b46f3d8999c4aef2a83403
MSiddiq10 <muhammadsiddiq@getweys.com>
2024-01-16 17:14:44 +0500

    create button component

---
+++ src/components/button.tsx
@@ -0,0 +1,15 @@
+interface Props {
+  name: string;
+}
+const Button = ({ name }: Props) => {
+  return (
+    <button
+      type="button"
+      className="text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2"
+    >
+      {name}
+    </button>
+  );
+};
+
+export default Button;
